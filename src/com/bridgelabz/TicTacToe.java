205d0d7fe0047e54b44bfca5e87635cb3016237e
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-30 08:37:15 +0530

    Create : Create a TicTac game

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static char[] newBoard();
+
+    {
+        char[] board = new char[10];
+        for (int i = 0; i < board.length; i++){
+            board[i] = ' '
+    }return board;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        newBoard();
+}
